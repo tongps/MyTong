3543f645ee42d85f0d90e2294ae532b36e946a79
tong <tong@angeladvance.com>
2018-12-12 14:43:57 +0700

    Replace Fragment Success

--- app/src/main/java/com/nawano/tong/mytong/MainFragment.java
+++ app/src/main/java/com/nawano/tong/mytong/MainFragment.java
@@ -2,10 +2,12 @@ package com.nawano.tong.mytong;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -20,10 +22,41 @@ public class MainFragment extends Fragment {
 
 
     @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+            // Register Controller
+
+        registerController();
+
+
+    }   //Main Method
+
+    private void registerController() {
+        TextView textView = getView().findViewById(R.id.txtRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                Replace Fragment
+                getActivity()
+                        .getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contentMainFragment, new RegisterFragment())
+                        .addToBackStack(null).commit();
+
+
+
+
+            }
+        });
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 
-}
+}   // Main Class
