ad1334af23166a55183a7f97795162bb4858d7e7
tong <tong@angeladvance.com>
2018-12-12 16:20:14 +0700

    Success 12Dec

--- app/src/main/java/com/nawano/tong/mytong/RegisterFragment.java
+++ app/src/main/java/com/nawano/tong/mytong/RegisterFragment.java
@@ -2,7 +2,9 @@ package com.nawano.tong.mytong;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +20,36 @@ public class RegisterFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+
+        createToolbar();
+
+    }
+
+    private void createToolbar() {
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle("Register");
+        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle(R.string.message_have_space);
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                getActivity().getSupportFragmentManager().popBackStack();
+
+            }
+        });
+
+
+    }
+
+//    Main Method
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
